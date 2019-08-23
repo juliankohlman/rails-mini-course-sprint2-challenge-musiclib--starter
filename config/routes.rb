97e07800fcec5102225ee042021b89ee23092c00
Julian Kohlman <juliankohlman@gmail.com>
2019-08-23 14:08:49 -0400

    Updates config porting old code over

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :albums, only: [:index, :show] do
-        resources :songs, only: [:index]
-      end
-      resources :artists, only: [:index, :show] do
-        resources :albums, only: [:index]
-      end
-      resources :songs, only: [:index, :show]
-      resources :users, only: [:index, :show, :create]
-    end
+      resources :albums, only: %i[index show] do
+				resources :songs, only: %i[index]
+			end
+
+			resources :artists, only: %i[index show] do
+				resources :albums, only: %i[index]
+			end
+			resources :songs, only: %i[index show]
+			resources :users, only: %i[index show create] do
+				resources :playlists, only: %i[create index]
+			end
+			resources :playlists, only: %i[show] do
+				resources :songs, only: %i[index create]
+			end
   end
 end
